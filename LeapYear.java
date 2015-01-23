d1a7c2133e7b9177fe0e26f90614227737777534
Shanzeh Khurram <SHANZEKHURRAM@calvisitor-10-105-138-61.CalVisitor.1918.Berkeley.EDU>
2015-01-23 14:45:46 -0800

    hi

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+ 
+        public static void main(String[] args) {
+               
+   
+                int year = 2000;
+               
+      
+               
+                if((year % 400 == 0) || ((year % 100 != 0) && (year % 4 == 0)))
+                        System.out.println("The year " + year + " is a leap year");
+                else
+                        System.out.println("The year " + year + " is not a leap year");
+        }
+}
